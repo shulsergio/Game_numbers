bd42bb36e8b9328fb3a313dc757f1a4eab28432c
shulsergio <101953752+shulsergio@users.noreply.github.com>
2022-08-05 22:01:32 +0300

    ver 1.5 (work ver)
    working

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
-const cityNames = ["odin", "dva", "trie", "chetire"];
-let compNames = "odin";
-
-const restCity = cityNames.filter((cityName) => compNames !== cityName);
-console.log(`cityName ${cityName}`);
-console.log(`compNames ${compNames}`);
-alert("vvely");
+x = 1;
+//число генерится
+let Num_comp = Math.floor(Math.random() * 100) + 1;
+// получим кнопку и сохраним ссылку на неё в переменную
+const btn = document.querySelector("#my-btn");
+// добавим к обработчик события click
+btn.onclick = function () {
+  alert(`Вы кликнули на кнопку!`);
+  text = +document.querySelector(".Input").value;
+  //  console.log(text);
+  document.querySelector(".ssi").innerHTML += " " + text;
+  //  console.log(text, Num_comp);
+  if (text === Num_comp) {
+    document.querySelector(".result").innerHTML = "YOU WIN";
+    alert("YOU WIN");
+    location.reload();
+  } else {
+    if (text > Num_comp) {
+      document.querySelector(".result").innerHTML = "is above";
+    } else {
+      document.querySelector(".result").innerHTML = "is low";
+    }
+  }
+};
