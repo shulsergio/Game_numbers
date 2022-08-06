758aedb3c7ad40e399fd61e0dcfeb0ede8c684ec
shulsergio <101953752+shulsergio@users.noreply.github.com>
2022-08-07 01:12:03 +0300

    ver 2.0 (eng add)

--- main.js
+++ main.js
@@ -4,30 +4,36 @@ let Num_comp = Math.floor(Math.random() * 100) + 1;
 const btn = document.querySelector(".buttonOk");
 const result = document.querySelector(".result");
 
+/*function Over() {
+  document.querySelector(".ssi").style.visibility = "visible";
+  document.querySelector(".ssi").innerHTML += " " + text;
+}*/
+
 btn.onclick = function () {
   x++;
-  alert(`Вы ввели цифру - ${x} раз!`);
+  alert(`You input - ${x} times!`);
   if (x === 11) {
-    result.innerHTML = "уже 10 раз ввели, вы проиграли";
+    result.innerHTML = "You LOSE! 10 times input";
     result.style.backgroundColor = "red";
     alert("YOU LOSE");
-    location.reload();
+
+    setTimeout(location.reload(), 100000000);
   }
   text = +document.querySelector(".Input").value;
   document.querySelector(".ssi").style.visibility = "visible";
   document.querySelector(".ssi").innerHTML += " " + text;
   //  console.log(text, Num_comp);
   if (text === Num_comp) {
-    result.innerHTML = "Ваше число больше";
+    result.innerHTML = "YOU WIN";
     result.style.backgroundColor = "Lime";
     alert("YOU WIN");
-    location.reload();
+    setTimeout(location.reload(), 100000000);
   } else {
     if (text > Num_comp) {
-      result.innerHTML = "Ваше число больше";
+      result.innerHTML = "Число больше/ Number above";
       result.style.backgroundColor = "IndianRed";
     } else {
-      result.innerHTML = "Ваше число меньше";
+      result.innerHTML = "Число меньше/ Number small";
       result.style.backgroundColor = "LightCoral";
     }
   }
